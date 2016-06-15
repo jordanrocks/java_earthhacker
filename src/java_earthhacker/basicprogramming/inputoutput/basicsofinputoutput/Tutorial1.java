a3b55a4a50409d552125ba32951662cf2b4654f4
Varun Arora <varun.arora@citiustech.com>
2016-06-15 18:03:25 +0530

    tutorial#1

---
+++ src/java_earthhacker/basicprogramming/inputoutput/basicsofinputoutput/Tutorial1.java
@@ -0,0 +1,23 @@
+package java_earthhacker.basicprogramming.inputoutput.basicsofinputoutput;
+
+import java.util.Scanner;
+
+public class Tutorial1 {
+	public static void main(String... args) {
+		Scanner in = new Scanner(System.in);
+		try {
+			int N = 0;
+			String S = null;
+
+			N = in.nextInt();
+			S = in.next();
+			if (N < 10) 
+				System.out.println(N * 2);
+			System.out.println(S);
+		} catch (Exception e){
+			
+		} finally {
+			in.close();
+		}
+	}
+}
